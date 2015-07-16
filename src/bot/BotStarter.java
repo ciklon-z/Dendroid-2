95673d6a51b86f0c4e3c1e37012472234a0074c9
Riley Bowling <ostrichking7@gmail.com>
2015-07-15 19:11:07 -0500

    basic outline

--- src/bot/BotStarter.java
+++ src/bot/BotStarter.java
@@ -17,11 +17,14 @@
 
 package bot;
 
+import field.Field;
+import field.ShapeType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.awt.Point;
 
 import moves.MoveType;
 
@@ -39,24 +42,79 @@ public class BotStarter {
 	public BotStarter() {}
 	
 	/**
-	 * Returns a random amount of random moves
+	 * Will eventually return the set of moves that sends the current shape 
+         * to the most optimal position based on the next shape and several metrics
 	 * @param state : current state of the bot
 	 * @param timeout : time to respond
 	 * @return : a list of moves to execute
 	 */
 	public ArrayList<MoveType> getMoves(BotState state, long timeout) {
-		ArrayList<MoveType> moves = new ArrayList<MoveType>();
-		Random rnd = new Random();
 		
-		int nrOfMoves = rnd.nextInt(41);
-		List<MoveType> allMoves = Collections.unmodifiableList(Arrays.asList(MoveType.values()));
-		for(int n=0; n<=nrOfMoves; n++) {
-			moves.add(allMoves.get(rnd.nextInt(allMoves.size())));
-		}
+                Field cField = state.getMyField();
+                ShapeType cShape = state.getCurrentShape();
+                ShapeType nShape = state.getNextShape();
+                ArrayList<MoveType> moves = new ArrayList<MoveType>();
+                
+                //the position of a shape is stored as a Point, not sure what Points are in java exactly will investigate
+                ArrayList<Point> cPos = getPos(cField, cShape);
+                ArrayList<Field> cFields = genFields(cField, cPos);
+                
+                ArrayList<Point> nPos;
+                ArrayList<ArrayList<Field>> nFields = null; //2D arraylist, syntax might be off
+                for (Field f : cFields) {
+                    nPos = getPos(f, nShape);
+                    nFields.add(genFields(f, nPos));
+                }
+                
+                /*at this point we'll have a 2D array of every possible field for the current and next shape that won't
+                have trapped empty spaces, next we'll need to do 2 things:
+                
+                1. compare them all and choose the best position based on the metrics system we create (this is what treeSearch() is for)
+                
+                2. convert the best position into move instructions to store in moves and return*/
+                
+                
+
 		
 		return moves;
 	}
 	
+        /**
+         * Returns all shape positions that don't have empty spaces underneath in an ArrayList
+         * @param field : any given field
+         * @param shape : any given shape
+         * @return : list of positions for the ShapeType to be in on the field that don't create trapped empty cells
+         */
+        public ArrayList<Point> getPos(Field field, ShapeType shape)
+        {
+            ArrayList<Point> pos = new ArrayList<Point>();
+            return pos;
+        }
+        
+        /**
+         * Returns all fields (board layouts) for a given ArrayList of positions
+         * @param field : any given field
+         * @param pos : any given ArrayList of positions (points)
+         * @return : an ArrayList with a generated field for each given position 
+         */
+        public ArrayList<Field> genFields(Field field, ArrayList<Point> pos)
+        {
+            ArrayList<Field> fields = new ArrayList<Field>();
+            return fields;
+        }
+        
+        /**
+         * searches the tree and finds the best position to use according to height 
+         * and potential block placement metrics
+         * @param Fields : the 2d ArrayList that is the tree
+         * @return the most optimal position
+         */
+        public Point treeSearch(ArrayList<ArrayList<Field>> Fields)
+        {
+            Point bestPos = new Point();
+            return bestPos; 
+        }
+        
 	public static void main(String[] args)
 	{
 		BotParser parser = new BotParser(new BotStarter());
